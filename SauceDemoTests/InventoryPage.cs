00f21ec7588dd1fe4c7dca9ef7c5a702fc271c69
James Smith <kaleburdev@gmail.com>
2024-09-12 23:04:22 -0400

    Add test for logging in and verifying a shopping cart link exists

---
+++ SauceDemoTests/InventoryPage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+
+namespace SauceDemoTests
+{
+    public class InventoryPage
+    {
+        private readonly IWebDriver _driver;
+
+        public InventoryPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public string Url = "https://www.saucedemo.com/inventory.html";
+
+        public IWebElement ShoppingCartLink => _driver.FindElement(By.Id("shopping_cart_container"));
+    }
+}
--- SauceDemoTests/LoginPageTests.cs
+++ SauceDemoTests/LoginPageTests.cs
@@ -1,11 +1,48 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace SauceDemoTests
 {
     [TestClass]
     public class LoginPageTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IWebDriver _driver;
+        private LoginPage _loginPage;
+        private InventoryPage _inventoryPage;
+
+        [TestInitialize]
+        public void Setup()
         {
+            _driver = new ChromeDriver();
+            _loginPage = new LoginPage(_driver);
+            _inventoryPage = new InventoryPage(_driver);
+        }
+
+        [TestMethod]
+        [DataRow("standard_user")]
+        [DataRow("locked_out_user")]
+        [DataRow("problem_user")]
+        [DataRow("performance_glitch_user")]
+        [DataRow("error_user")]
+        [DataRow("visual_user")]
+        public void TestMethod1(string username)
+        {
+            var password = "secret_sauce";
+
+            _driver.Navigate().GoToUrl(_loginPage.Url);
+            _loginPage.UsernameTextbox.SendKeys(username);
+            _loginPage.PasswordTextbox.SendKeys(password);
+            _loginPage.LoginButton.Click();
+            var shoppingCartLinkDisplayed = _inventoryPage.ShoppingCartLink.Displayed;
+
+            Assert.IsTrue(shoppingCartLinkDisplayed);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _driver.Quit();
+            _driver.Dispose();
         }
     }
 }
