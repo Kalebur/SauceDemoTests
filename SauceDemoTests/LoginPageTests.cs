70efbc659063a5f832558112563fee40da5532cb
James Smith <kaleburdev@gmail.com>
2024-09-19 20:48:45 -0400

    Extract logins to a method and update logintest accordingly

--- SauceDemoTests/LoginPageTests.cs
+++ SauceDemoTests/LoginPageTests.cs
@@ -9,6 +9,7 @@ namespace SauceDemoTests
         private IWebDriver _driver;
         private LoginPage _loginPage;
         private InventoryPage _inventoryPage;
+        private SeleniumHelpers _seleniumHelpers;
 
         [TestInitialize]
         public void Setup()
@@ -16,26 +17,15 @@ namespace SauceDemoTests
             _driver = new ChromeDriver();
             _loginPage = new LoginPage(_driver);
             _inventoryPage = new InventoryPage(_driver);
+            _seleniumHelpers = new SeleniumHelpers(_driver, _inventoryPage, _loginPage);
         }
 
         [TestMethod]
-        [DataRow("standard_user")]
-        [DataRow("locked_out_user")]
-        [DataRow("problem_user")]
-        [DataRow("performance_glitch_user")]
-        [DataRow("error_user")]
-        [DataRow("visual_user")]
-        public void TestMethod1(string username)
+        public void LoginTest()
         {
-            var password = "secret_sauce";
+            _seleniumHelpers.LoginAs("standard_user");
 
-            _driver.Navigate().GoToUrl(_loginPage.Url);
-            _loginPage.UsernameTextbox.SendKeys(username);
-            _loginPage.PasswordTextbox.SendKeys(password);
-            _loginPage.LoginButton.Click();
-            var shoppingCartLinkDisplayed = _inventoryPage.ShoppingCartLink.Displayed;
-
-            Assert.IsTrue(shoppingCartLinkDisplayed);
+            Assert.AreEqual(_inventoryPage.Url, _driver.Url);
         }
 
         [TestCleanup]
---
+++ SauceDemoTests/SeleniumHelpers.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+
+namespace SauceDemoTests
+{
+    public class SeleniumHelpers
+    {
+        private readonly IWebDriver _driver;
+        private readonly InventoryPage _inventoryPage;
+        private readonly LoginPage _loginPage;
+        private const string _password = "secret_sauce";
+
+        public SeleniumHelpers(IWebDriver driver, InventoryPage inventoryPage, LoginPage loginPage)
+        {
+            _driver = driver;
+            _inventoryPage = inventoryPage;
+            _loginPage = loginPage;
+        }
+
+        public void NavigateTo(string url)
+        {
+            _driver.Navigate().GoToUrl(url);
+        }
+
+        public void LoginAs(string username)
+        {
+            NavigateTo(_inventoryPage.Url);
+            _loginPage.UsernameTextbox.SendKeys(username);
+            _loginPage.PasswordTextbox.SendKeys(_password);
+            _loginPage.LoginButton.Click();
+        }
+    }
+}
